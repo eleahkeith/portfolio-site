388d27ae0e764489ca583d489fd419f671e80408
Leah Keith <eleahkeith@gmail.com>
2021-06-06 16:09:22 +0100

    style: fix some styling for desktop/mid size devices

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -10,7 +10,7 @@ const About = () => {
       <div className="component-body">
         <div className="about-text">
           <div className="about-section" id="s1">
-            <h2>Welcome!</h2>
+            <h2 className="about-subhead">Welcome!</h2>
             <p>
               My name is Leah and I'm a front-end software engineer. I am
               passionate about creating responsive, accessible websites for all
@@ -20,7 +20,7 @@ const About = () => {
             </p>
           </div>
           <div className="about-section" id="s2">
-            <h2>Career</h2>
+            <h2 className="about-subhead">Career</h2>
             <p>
               My software development career began in early 2020 when I attended
               a web development bootcamp at <a href="#">23 Code Street</a> based
@@ -30,7 +30,7 @@ const About = () => {
             </p>
           </div>
           <div className="about-section" id="s3">
-            <h2>Philosophy</h2>
+            <h2 className="about-subhead">Philosophy</h2>
             <p>
               Because I've spent so many years working with people and clients,
               I take a people-first approach to development. Tech exists to
@@ -39,7 +39,7 @@ const About = () => {
             </p>
           </div>
           <div className="about-section" id="s4">
-            <h2>Me IRL</h2>
+            <h2 className="about-subhead">Me IRL</h2>
             <p>
               Outside of development, I'm an avid knitter, gamer, and baker. I
               am from the US originally, but I now live in the UK with my dog
@@ -56,8 +56,14 @@ const About = () => {
           Want to see photos of my dog?
         </div>
         <div className="photo-carousel">
-          <div className="carousel-dummy"></div>
-          <div className="carousel-dummy"></div>
+          <div className="carousel tier1">
+            <div className="carousel-dummy"></div>
+            <div className="carousel-dummy"></div>
+          </div>
+          <div className="carousel tier2">
+            <div className="carousel-dummy"></div>
+            <div className="carousel-dummy"></div>
+          </div>
         </div>
       </div>
     </div>
